da62434c53e1e981ed217059e0aac3598746772f
Huguinho <up201404587@fe.up.pt>
2017-05-22 09:11:18 +0100

    befriends done

--- proto/actions/users/friendship.php
+++ proto/actions/users/friendship.php
@@ -1,16 +1,14 @@
 <?php
 include_once('../../config/init.php');
 
-if(isset($_POST['friendship-button'])) 
-{ 
+
     global $dbh;
 	$id1 = $_SESSION['id'];
-	$id2= $_GET['user-id'];
+	$id2= $_POST['user2-id'];
 	$stmt = $dbh->prepare('INSERT INTO "Friendship" VALUES ( :id1 , :id2 ,false)');
 	$stmt->bindParam(':id1', $id1);
 	$stmt->bindParam(':id2', $id2);
 	$stmt->execute();
-}
 
 header('Location: ' . $_SERVER['HTTP_REFERER']);
 ?>
